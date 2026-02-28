5d4d6a68467471289a6a221ca4cce97521621e82
0motech <7motech@gmail.com>
2026-02-28 09:08:51 +0200

    Update MainLayout

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,9 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import Subscription from "@/pages/SubscriptionPlans/Subscription";
-import Settings from "@/pages/Settings/Settings";
-import Profile from "@/pages/Profile/Profile";
+
+
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
@@ -12,7 +11,6 @@ const MainLayout = () => {
       <main className="flex-grow">
         <Outlet  />
       </main>
-
       <Footer />
     </div>
   );
