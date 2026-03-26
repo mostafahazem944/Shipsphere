296ecfb2bb8b80ffd8db84f8a2beb4927f82424e
Mohammed Elalimy <mohammed.m.elalimy@gmail.com>
2026-03-26 09:46:05 +0200

    feat:Add cookie-parser middleware to fix undefined refreshToken in req.cookies and enable proper refresh token handling

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,29 @@
-
-
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/DB/connection';
 import { env } from './config/env/env';
 import './config/DB/Models';
+import cookieParser from 'cookie-parser';
 
 import { errorHandler } from './middleware/Error/error.middleware';
 import routerHandler from './utils/RouterHandler/routerHandler';
 
 const bootstrap = async (app: express.Application): Promise<void> => {
-
   app.use(
     cors({
-      origin: env.ALLOWED_ORIGINS
-        ? env.ALLOWED_ORIGINS.split(',').map((o) => o.trim())
-        : '*',
-      credentials: true,
+      origin: env.ALLOWED_ORIGINS ? env.ALLOWED_ORIGINS.split(',').map((o) => o.trim()) : '*',
+      credentials: true
     })
   );
 
-  app.use("/api/stripe/webhook", express.raw({ type: "application/json" }));
+  app.use('/api/stripe/webhook', express.raw({ type: 'application/json' }));
   app.use(express.json());
-    
+  app.use(cookieParser());
 
   await connectDB();
 
   await routerHandler(app);
- 
+
   app.use(errorHandler);
 };
 
